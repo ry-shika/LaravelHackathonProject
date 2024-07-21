c758c27f93c3afaf3ea7e9d5310d0d382801b9cb
ry-shika <ry.shikanai@gmail.com>
2024-07-21 12:22:18 +0900

    add: create-welcomepage-css ver2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,10 @@
         <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
         </style>
     </head>
-<!-- <body>
-</head> -->
+    
     <body>
         <div class="app">
-            <div class="header">Welcome to the Folders List</div>
-
+            <div class="header"><h1>Home</h1></div>
 
             <!-- 成功メッセージの表示 -->
             @if (session('success'))
@@ -23,133 +21,78 @@
 
             <div class="body">
                 <div class="sidebar">
-                    <div class="homeButton">
-                        ホームボタン
-                    </div>
+                    <form action="/welcome" >
+                        @csrf
+                        <button class="homeButton">Home</button>
+                    </form>
                     <div class="searchButton">
-                        検索ボタン
+                        <p class="search">Search<p>
                     </div>
                     <form action="/postPage" >
                         @csrf
-                        <button class="postButton">投稿ボタン</button>
+                        <button class="postButton">Post</button>
                     </form>
+                    <div class="loginButton">
+                        <p class="login">login<p>
+                    </div>
                 </div>
 
-                <div class="content">
-                    <div class="flex-content">
-                        <!-- フォルダデータのテーブル -->
-                        <table border="1">
-                            <thead>
-                                <tr>
-                                    <!-- テーブルのヘッダー -->
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Place</th>
-                                    <th>Color</th>
-                                    <th>Shape</th>
-                                    <th>Time</th>
-                                    <th>Created At</th>
-                                    <th>Updated At</th>
-                                    <th>Comments</th>
-                                    <th>Add Comment</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <!-- 各フォルダの情報をテーブルの行として表示 -->
-                                @foreach($folders as $folder)
-                                <tr>
-                                    <!-- 各フォルダのプロパティを表示 -->
-                                    <td>{{ $folder->id }}</td>
-                                    <td>{{ $folder->name }}</td>
-                                    <td>{{ $folder->place }}</td>
-                                    <td>{{ $folder->color }}</td>
-                                    <td>{{ $folder->shape }}</td>
-                                    <td>{{ $folder->time }}</td>
-                                    <td>{{ $folder->created_at }}</td>
-                                    <td>{{ $folder->updated_at }}</td>
-                                    <td>
-                                        <!-- コメントを表示 -->
-                                        @foreach($folder->comments as $comment)
-                                            <div>{{ $comment->comment }}</div>
-                                        @endforeach
-                                    </td>
-                                    <td>
-                                        <!-- コメント入力フォーム -->
-                                        <form action="/comments" method="POST">
-                                            @csrf
-                                            <input type="hidden" name="folder_id" value="{{ $folder->id }}">
-                                            <input type="text" name="comment" placeholder="Add a comment" required>
-                                            <button type="submit">Submit</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                <div class="flex-content">
+                    <!-- フォルダデータのテーブル -->
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <!-- テーブルのヘッダー -->
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Place</th>
+                                <th>Color</th>
+                                <th>Shape</th>
+                                <th>Time</th>
+                                <th>Created At</th>
+                                <th>Updated At</th>
+                                <th>Comments</th>
+                                <th>Add Comment</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- 各フォルダの情報をテーブルの行として表示 -->
+                            @foreach($folders as $folder)
+                            <tr>
+                                <!-- 各フォルダのプロパティを表示 -->
+                                <td>{{ $folder->id }}</td>
+                                <td>{{ $folder->name }}</td>
+                                <td>{{ $folder->place }}</td>
+                                <td>{{ $folder->color }}</td>
+                                <td>{{ $folder->shape }}</td>
+                                <td>{{ $folder->time }}</td>
+                                <td>{{ $folder->created_at }}</td>
+                                <td>{{ $folder->updated_at }}</td>
+                                <td>
+                                    <!-- コメントを表示 -->
+                                    @foreach($folder->comments as $comment)
+                                        <div>{{ $comment->comment }}</div>
+                                    @endforeach
+                                </td>
+                                <td>
+                                    <!-- コメント入力フォーム -->
+                                    <form action="/comments" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="folder_id" value="{{ $folder->id }}">
+                                        <input type="text" name="comment" placeholder="Add a comment" required>
+                                        <button type="submit">Submit</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div class="radius_post">
                         <form action="/postPage" >
                             @csrf
-                            <button type="submit">Submit</button>
+                            <button class="submit">Post</button>
                         </form>
-                        <p>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                            ここはスクロール可能です。<br>
-                        </p>
                     </div>
-                    <div class="fixed-content">コンテンツ2</div>
                 </div>
             </div>
         </div>
