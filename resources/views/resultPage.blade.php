84f482deb0f6a0e5cd84afddddadd67e486d8399
ry-shika <ry.shikanai@gmail.com>
2024-07-21 14:40:38 +0900

    add: create-resultPage-css

--- resources/views/resultPage.blade.php
+++ resources/views/resultPage.blade.php
@@ -1,19 +1,48 @@
 <html>
     <head>
-        <title>投稿内容確認</title>
+        <title>PostResult</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="{{ asset('css/resultPage.css') }}">
+        </style>
     </head>
     <body>
-        <h1>POST結果</h1>
-        <div>種類：{{$name}}</div>
-        <div>拾った場所：{{$place}}</div>
-        <div>色：{{$color}}</div>
-        <div>形状：{{$shape}}</div>
-        <div>拾った日時：{{$time}}</div>
-        <a href="{{action('postController@movePostPage')}}">投稿画面に戻る</a>
-        
-        <form action="/welcome" >
-            @csrf
-            <button type="submit">ホームページに戻る</button>
-        </form>
+        <div class="app">
+            <div class="header"><h1>PostResult</h1></div>
+
+            <div class="body">
+                <div class="sidebar">
+                    <form action="/welcome" >
+                        @csrf
+                        <button class="homeButton">Home</button>
+                    </form>
+                    <div class="searchButton">
+                        <p class="search">Search<p>
+                    </div>
+                        <form action="/postPage" >
+                            @csrf
+                            <button class="postButton">Post</button>
+                        </form>
+                    <div class="loginButton">
+                        <p class="login">login<p>
+                    </div>
+                </div>
+
+                <div class="flex-content">
+
+                    <h2>入力内容</h2>
+                    <div>種類：{{$name}}</div>
+                    <div>拾った場所：{{$place}}</div>
+                    <div>色：{{$color}}</div>
+                    <div>形状：{{$shape}}</div>
+                    <div>拾った日時：{{$time}}</div>
+                    <a href="{{action('postController@movePostPage')}}">投稿画面に戻る</a>
+                    
+                    <form action="/welcome" >
+                        @csrf
+                        <button type="submit">ホームページに戻る</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
